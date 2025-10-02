fa439badc3a573a7e20e65dd7061614b5ef63d5c
Aidan van Tonder <aidanvantonder@gmail.com>
2025-10-02 04:08:16 +0200

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,63 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[BepInPlugin("com.example.skiptro", "Skiptro Mod", "1.0.0")]
+public class Main : BaseUnityPlugin
+{
+    public static ManualLogSource BaseLogger = null!;
+    public static PrefixedLogger LoggerInstance = null!;
+    private bool skipped = false;
+
+    void Awake()
+    {
+        BaseLogger = Logger;
+        LoggerInstance = new PrefixedLogger(BaseLogger, this);
+    }
+
+    public void Start()
+    {
+        LoggerInstance.LogMessage($"For more log info, set LogLevels to All.\n");
+        LoggerInstance.LogMessage($"Skiptro is now active.");
+
+        LoggerInstance.LogInfo($"Initialising Harmony loggers..."); // Important to do this before using Harmony, so that the loggers are ready to go.
+        LoggerInstance.LogDebug($"> PreRunScriptTryLore...");
+        PreRunScriptTryLorePrefix.InitializeLogger();
+        LoggerInstance.LogInfo($"Harmony logs successfully initialised.\n");
+
+        LoggerInstance.LogInfo($"Creating Harmony patches and patching necessary methods...");
+        LoggerInstance.LogDebug($"> PreRunScriptTryLore...");
+        Harmony.CreateAndPatchAll(typeof(PreRunScriptTryLorePrefix));
+        LoggerInstance.LogInfo($"Harmony patches successful.\n");
+
+        LoggerInstance.LogMessage($"Skiptro successfully initialised.\n");
+    }
+
+    void Update()
+    {
+        if (skipped) return;
+
+        if (GameObject.Find("Warning")?.GetComponent<Button>() is Button button)
+        {
+            LoggerInstance.LogInfo("Found 'Warning' button, invoking click...");
+            button.onClick.Invoke();
+            skipped = true;
+        }
+        else LoggerInstance.LogWarning("Found 'Warning' object, but no Button component attached, or no warning object found.\n");
+    }
+}
+
+[HarmonyPatch(typeof(PreRunScript), nameof(PreRunScript.TryLore))]
+public class PreRunScriptTryLorePrefix
+{
+    private static PrefixedLogger LoggerInstance = null!;
+    public static void InitializeLogger() => LoggerInstance = new PrefixedLogger(Main.BaseLogger, typeof(PreRunScriptTryLorePrefix));
+    static bool Prefix()
+    {
+        LoggerInstance.LogInfo($"Skipping intro lore...\n");
+        return false;
+    }
+}
---
+++ PrefixedLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BepInEx.Logging;
+
+public class PrefixedLogger
+{
+    private readonly ManualLogSource _baseLogger;
+    private string prefix;
+
+    public PrefixedLogger(ManualLogSource baseLogger, object owner)
+    {
+        _baseLogger = baseLogger;
+        prefix = $"{owner.GetType().Name}: ";
+    }
+
+    public PrefixedLogger(ManualLogSource baseLogger, Type ownerType)
+    {
+        _baseLogger = baseLogger;
+        prefix = $"{ownerType.Name}: ";
+    }
+
+    public void LogMessage(string message) => _baseLogger.LogMessage(prefix + message);
+    public void LogInfo(string message) => _baseLogger.LogInfo(prefix + message);
+    public void LogDebug(string message) => _baseLogger.LogDebug(prefix + message);
+    public void LogWarning(string message) => _baseLogger.LogWarning(prefix + message);
+    public void LogError(string message) => _baseLogger.LogError(prefix + message);
+}
